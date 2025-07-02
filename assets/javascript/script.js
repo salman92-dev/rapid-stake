14f2a12d3308cd79c5678d28f2900468cea3f704
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2025-07-02 21:44:53 +0500

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarLinks = document.querySelector(".center-menu");
+
+navbarToggle.addEventListener("click", () => {
+  navbarToggle.classList.toggle("active");
+ 
+  navbarLinks.classList.toggle("active");
+});
+
+
